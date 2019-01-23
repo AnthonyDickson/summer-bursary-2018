10c9c6f6d35ff592f1f814a578aa5120a380cf61
Anthony Dickson <eight0153@gmail.com>
2019-01-24 12:31:21 +1300

    add more docstrings and add planes + ray/plane intersection

--- raytracerthing.py
+++ raytracerthing.py
@@ -1,44 +1,86 @@
 import numpy as np
 
 
-class Vec3D(np.ndarray):
+class Vec3f(np.ndarray):
+    """Represents a row vector with three elements: x, y, and z."""
+
     @staticmethod
     def zero():
-        return Vec3D([0, 0, 0])
+        """Create a zero vector.
+
+        Returns:
+            a zero vector with three elements.
+        """
+        return Vec3f([0, 0, 0])
 
     def __new__(cls, input_array):
         obj = np.asarray(input_array).view(cls)
 
-        assert obj.shape == (3,), "Vec3D expects an 1-D input of shape (1, ), instead got an input of shape %s" % obj.shape
+        assert obj.shape == (3,), "Vec3D expects an 1-D input of " \
+                                  "shape (3, ), instead got an input of " \
+                                  "shape %s" % obj.shape
 
-        return obj
+        return obj.astype(np.float64)
 
     @property
     def x(self):
+        """The first element of the vector, x.
+
+        Returns:
+            the first element of the vector.
+        """
         return self[0]
 
     @property
     def y(self):
+        """The second element of the vector, y.
+
+        Returns:
+            the second element of the vector.
+        """
         return self[1]
 
     @property
     def z(self):
+        """The third element of the vector, z.
+
+        Returns: the third element of the vector.
+        """
         return self[2]
 
     def normalise(self):
-        if np.array_equal(self, Vec3D.zero()):
+        """Normalise the vector.
+
+        Returns:
+            the corresponding unit vector, or a zero vector if the
+            initial vector was also a zero vector.
+        """
+        if np.array_equal(self, Vec3f.zero()):
             return self
 
         return self / np.sqrt(np.square(self).sum())
 
 
 class Ray:
+    """Represents a ray object used in ray tracing."""
+
     def __init__(self, origin=None, direction=None):
+        """Create a ray.
+
+        Arguments:
+            origin: The point in space from where the ray originates. If set to
+                    None then origin is set to the point (0, 0, 0).
+            direction: The direction that the ray is travelling in. If set to
+                       None then the direction is set to point in the positive
+                       z direction, (0, 0, 1). The direction will be
+                       normalised.
+
+        """
         if origin is None:
-            origin = Vec3D.zero()
+            origin = Vec3f.zero()
 
         if direction is None:
-            direction = Vec3D.zero()
+            direction = Vec3f.zero()
 
         assert origin.shape == (3, )
         assert direction.shape == (3, )
@@ -47,15 +89,123 @@ class Ray:
 
         self.origin = origin
         self.direction = direction
+        self.epsilon = np.finfo(type(self.origin.x)).eps
 
     def get_point(self, t=0):
+        """Get the point along the ray for a given value of t.
+
+        Returns:
+            the point along the ray for the given value of t.
+        """
         return self.origin + t * self.direction
+
+    def intersects(self, other):
+        """Check if the ray intersects another object.
+
+        Arguments:
+            other: The object to check intersection with.
+
+        Returns:
+            True if the ray and the object intersect, otherwise False.
+        """
+        if isinstance(other, Plane3D):
+            return self.intersects_plane(other)
+
+    def intersects_plane(self, plane):
+        """Check if the ray intersects a plane.
+
+        Based on code from: https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-plane-and-ray-disk-intersection
+
+        Arguments:
+            plane: The plane to check intersection with.
+
+        Returns:
+            True if the plane and the object intersect, otherwise False.
+        """
+        denom = self.direction.dot(plane.norm)
+        parallel = denom < self.epsilon
+
+        if parallel:
+            return (plane.origin - self.origin).dot(plane.norm) < self.epsilon
+        else:
+            return True
+
+
+class Plane3D:
+    """A plane in 3-D space."""
+
+    def __init__(self, origin=None, norm=None):
+        """Create a plane in 3-D space.
+
+        Arguments:
+            origin: The point in space from where the plane originates. If set
+                    to None then origin is set to the point (0, 0, 0).
+            norm: The vector normal to the plane, (i.e. the vector pointing
+                  'upwards' perpendicularly from the plane). If set to None
+                  then the norm is set to point in the positive z direction,
+                  (0, 0, 1).
+
+        Raises:
+            AssertionError: if none of the elements in `norm` are non-zero.
+        """
+        if origin is None:
+            origin = Vec3f.zero()
+
+        if norm is None:
+            norm = Vec3f([0, 0, 1])
+
+        for vector in [origin, norm]:
+            assert vector.shape == (3, ), "Plane3D expects a vector with " \
+                                          "the shape (3, ), instead got %s." \
+                                          % vector.shape
+
+        assert np.count_nonzero(norm) > 0, "At least one element of `norm` " \
+                                           "must be non-zero."
+
+        self.origin = origin
+        self.norm = norm.normalise()
+        self.d = origin.dot(norm)
+
+    def point_intersects(self, point):
+        """Check if a point intersects (i.e. lies on) a plane.
+
+        Arguments:
+            point: The point in 3-D space to check intersection for.
+
+        Returns:
+            True if the point lies on the plane, otherwise False.
+        """
+
+        return self.origin.dot(point) == self.d
 
 
 class RayTracerThing:
+    """This thing does some stuff."""
+
     def __init__(self, input_shape, output_shape):
+        """Create a ray tracer thing (need to think of a better name).
+
+        Arguments:
+            input_shape: The shape of the input image.
+            output_shape: The shape of the detector array.
+        """
         self.input_shape = input_shape
         self.output_shape = output_shape
 
     def forward(self, X):
-        assert X.shape == self.input_shape
+        """Perform a 'forward pass' of the ray tracer thing.
+
+        Arguments:
+            X: The input image, must be the same shape as `input_shape`.
+
+        Returns:
+            2-D array of values that is the same shape as `output_shape`.
+
+        Raises:
+            AssertionError: if the shape of `X` does not match `input_shape`.
+        """
+        assert X.shape == self.input_shape, "Expected input to be of the " \
+                                            "shape %s, instead got %s." \
+                                            % (self.input_shape, X.shape)
+
+        output = np.zeros(shape=self.output_shape)
--- tests.py
+++ tests.py
@@ -2,31 +2,31 @@ import unittest
 
 import numpy as np
 
-from raytracerthing import RayTracerThing, Vec3D, Ray
+from raytracerthing import RayTracerThing, Vec3f, Ray, Plane3D
 
 
 class TestVec3D(unittest.TestCase):
 
     def test_init(self):
         expected = [0, 0, 0]
-        actual = Vec3D.zero()
+        actual = Vec3f.zero()
 
         self.assertTrue(np.array_equal(expected, actual))
 
     def test_init_raises_error(self):
         with self.assertRaises(AssertionError):
-            Vec3D([1])
-            Vec3D([1, 2, 3, 4])
+            Vec3f([1])
+            Vec3f([1, 2, 3, 4])
 
     def test_xyz_accessors(self):
-        v = Vec3D([1, 2, 3])
+        v = Vec3f([1, 2, 3])
 
         self.assertEqual(v.x, 1)
         self.assertEqual(v.y, 2)
         self.assertEqual(v.z, 3)
 
     def test_normalise_zero(self):
-        v = Vec3D.zero()
+        v = Vec3f.zero()
 
         expected = [0, 0, 0]
         actual = v.normalise()
@@ -34,7 +34,7 @@ class TestVec3D(unittest.TestCase):
         self.assertTrue(np.array_equal(expected, actual))
 
     def test_normalise_simple(self):
-        v = Vec3D([0, 0, 1])
+        v = Vec3f([0, 0, 1])
 
         expected = [0, 0, 1]
         actual = v.normalise()
@@ -42,12 +42,42 @@ class TestVec3D(unittest.TestCase):
         self.assertTrue(np.array_equal(expected, actual))
 
     def test_normalise(self):
-        v = Vec3D([1, 2, 3])
+        v = Vec3f([1, 2, 3])
 
         expected = [0.26726124, 0.53452248, 0.80178373]
         actual = v.normalise()
 
         self.assertTrue(np.allclose(expected, actual))
+
+
+class TestPlane3D(unittest.TestCase):
+
+    def test_init(self):
+        plane = Plane3D()
+
+        expected = 0
+        actual = plane.d
+
+        self.assertEqual(expected, actual)
+
+        plane = Plane3D(origin=Vec3f([1, 0, 1]),
+                        norm=Vec3f([1, 0, 0]))
+
+        expected = 1
+        actual = plane.d
+
+        self.assertEqual(expected, actual)
+
+    def test_init_raises_error(self):
+        with self.assertRaises(AssertionError):
+            Plane3D(origin=Vec3f.zero(),
+                    norm=Vec3f.zero())
+
+    def test_point_intersection(self):
+        plane = Plane3D()  # any point with z=0 should intersect.
+        point = Vec3f([1, 3, 0])
+
+        self.assertTrue(plane.point_intersects(point))
 
 
 class TestRay(unittest.TestCase):
@@ -61,7 +91,7 @@ class TestRay(unittest.TestCase):
         self.assertTrue(np.array_equal(expected, actual))
 
     def test_ray_position(self):
-        ray = Ray(direction=Vec3D([0, 0, 1]))
+        ray = Ray(direction=Vec3f([0, 0, 1]))
 
         expected = np.array([0, 0, 1])
         actual = ray.get_point(t=1)
@@ -72,6 +102,39 @@ class TestRay(unittest.TestCase):
         actual = ray.get_point(t=2.5)
 
         self.assertTrue(np.array_equal(expected, actual))
+
+    def test_ray_plane_single_intersection(self):
+        ray = Ray(origin=Vec3f([0, 0, -1]),
+                  direction=Vec3f([0, 0, 1]))
+
+        plane = Plane3D(origin=Vec3f.zero(),
+                        norm=Vec3f([0, 0, 1]))
+
+        self.assertTrue(ray.intersects_plane(plane))
+
+        # This ray should be close to parallel, but still intersect.
+        ray = Ray(origin=Vec3f([0, 0, -1]),
+                  direction=Vec3f([10000000, 0, 1]))
+
+        self.assertTrue(ray.intersects_plane(plane))
+
+    def test_ray_plane_contained_intersection(self):
+        ray = Ray(origin=Vec3f.zero(),
+                  direction=Vec3f([1, 0, 0]))
+
+        plane = Plane3D(origin=Vec3f.zero(),
+                        norm=Vec3f([0, 0, 1]))
+
+        self.assertTrue(ray.intersects_plane(plane))
+
+    def test_ray_plane_no_intersection(self):
+        ray = Ray(origin=Vec3f([0, 0, -1]),
+                  direction=Vec3f([1, 0, 0]))
+
+        plane = Plane3D(origin=Vec3f.zero(),
+                        norm=Vec3f([0, 0, 1]))
+
+        self.assertFalse(ray.intersects_plane(plane))
 
 
 class TestRaytracerThing(unittest.TestCase):
